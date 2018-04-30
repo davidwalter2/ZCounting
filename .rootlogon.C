87dfe3ba0eee6c25735c39c9a92bc4c1c43c34c7
Jakob <jakob.salfeld@cern.ch>
2018-04-30 17:17:25 +0200

    v4 of testZCounting.py, directory to be tidied up

---
+++ .rootlogon.C
@@ -0,0 +1,9 @@
+{     
+  gROOT->Macro("Utils/CPlot.cc+");
+  gROOT->Macro("Utils/MitStyleRemix.cc+");
+  gROOT->Macro("Utils/CEffUser1D.cc+");
+  gROOT->Macro("Utils/CEffUser2D.cc+");
+              
+  // Show which process needs debugging
+  gInterpreter->ProcessLine(".! ps |grep root.exe");
+}
